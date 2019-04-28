c06b47973b3955eae54f1ea5baa29814d9492b19
Sutton Yamanashi <syamanashi@ast.com>
2019-04-28 03:30:04 -0600

    0309.Controlling the Layout with @angularflex-layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -24,7 +25,7 @@ import { WelcomeComponent } from './welcome/welcome.component';
     PastTrainingsComponent,
     WelcomeComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MaterialModule],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MaterialModule, FlexLayoutModule],
   providers: [],
   bootstrap: [AppComponent],
 })
