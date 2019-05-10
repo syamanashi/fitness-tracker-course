1bc6886b3ed60597ba79dfe19e613e2b3716e6a1
Sutton Yamanashi <syamanashi@ast.com>
2019-05-10 12:28:28 -0400

    0507.Injecting & Using the Training Service

--- src/app/training.service.ts
+++ src/app/training.service.ts
@@ -5,13 +5,17 @@ import { Exercise } from './training/exercise.model';
   providedIn: 'root',
 })
 export class TrainingService {
-  availableExercises: Exercise[] = [
+  private exercises: Exercise[] = [
     { id: 'crunches', name: 'Crunches', duration: 30, calories: 8 },
     { id: 'touch-toes', name: 'Touch Toes', duration: 180, calories: 15 },
     { id: 'side-lunges', name: 'Side Lunges', duration: 120, calories: 18 },
     { id: 'burpees', name: 'Burpees', duration: 60, calories: 8 },
     { id: 'standing-bow', name: 'Standing Bow', duration: 200, calories: 20 },
   ];
+
+  get availableExercises() {
+    return this.exercises.slice(); // returns a new array created from an instance of this.exercises.
+  }
 
   constructor() {}
 }
