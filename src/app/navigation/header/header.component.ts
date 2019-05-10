514216f3decedf7721e2e609b989ca5b5c1363b9
Sutton Yamanashi <syamanashi@ast.com>
2019-05-10 08:01:39 -0400

    Remove need for subscription handling in Header Component

--- src/app/navigation/header/header.component.ts
+++ src/app/navigation/header/header.component.ts
@@ -1,28 +1,17 @@
-import { Component, OnInit, Output, EventEmitter, OnDestroy } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { AuthService } from 'src/app/auth/auth.service';
-import { takeWhile } from 'rxjs/operators';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent implements OnInit, OnDestroy {
+export class HeaderComponent implements OnInit {
   @Output() sideNavToggle = new EventEmitter<void>();
-  isAuth = false;
-  componentActive = true;
 
-  constructor(private authService: AuthService) {}
+  constructor(public authService: AuthService) {}
 
-  ngOnInit() {
-    this.authService.authChange.pipe(takeWhile(() => this.componentActive)).subscribe((authStatus: boolean) => {
-      this.isAuth = authStatus;
-    });
-  }
-
-  ngOnDestroy() {
-    this.componentActive = false;
-  }
+  ngOnInit() {}
 
   onToggleSidenav() {
     this.sideNavToggle.emit();
