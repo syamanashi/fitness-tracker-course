1f16f5003c88e2f0bd6a9a70a37244b13ca1631e
Sutton Yamanashi <syamanashi@ast.com>
2019-05-12 14:11:39 -0400

    0608.Listening to Snapshot Changes (of Firestore, incl. Metadata)

--- src/app/training/new-training/new-training.component.ts
+++ src/app/training/new-training/new-training.component.ts
@@ -5,6 +5,7 @@ import { AngularFirestore } from '@angular/fire/firestore';
 import { TrainingService } from 'src/app/training.service';
 import { Exercise } from '../exercise.model';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-new-training',
@@ -12,12 +13,25 @@ import { Observable } from 'rxjs';
   styleUrls: ['./new-training.component.scss'],
 })
 export class NewTrainingComponent implements OnInit {
-  availableExercises: Observable<any[]>;
+  availableExercises: Observable<Exercise[]>;
 
   constructor(public trainingService: TrainingService, private db: AngularFirestore) {}
 
   ngOnInit() {
-    this.availableExercises = this.db.collection('availableExercises').valueChanges();
+    this.availableExercises = this.db
+      .collection('availableExercises')
+      .snapshotChanges()
+      .pipe(
+        map(docArray => {
+          return docArray.map(doc => {
+            return {
+              id: doc.payload.doc.id,
+              ...doc.payload.doc.data(),
+            } as Exercise;
+          });
+        })
+      );
+    // .subscribe(result => console.log('snapshotChnges', result));
   }
 
   onStartTraining(form: NgForm) {
